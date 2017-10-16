74fd8d44bd197f65c7cdbc2122d52d71d12e9c9c
xkovari6 <xkovari6@titan23.ad.fi.muni.cz>
2017-10-16 14:22:27 +0200

    Task02

--- eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
+++ eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
@@ -33,37 +33,34 @@ public class Task02 extends AbstractTestNGSpringContextTests {
                 
                 EntityManager em1 = emf.createEntityManager();
 		em1.getTransaction().begin();
-                product1 = new Product();
-                product1.setName("Flashlight");
-                //p1.addCategory(cat);
-                em1.persist(product1);
-                product2 = new Product();
-                product2.setName("Kitchen robot");
-                //p2.addCategory(cat1);
-                em1.persist(product2);
-                product3 = new Product();
-                product3.setName("Plate");
-                //p3.addCategory(cat1);
-                em1.persist(product3);
-		em1.getTransaction().commit();
-		em1.close();
                 
-                EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		category1 = new Category();
+                category1 = new Category();
 		category1.setName("Electro");
                 category1.addProduct(product1);
-		em.persist(category1);
+		em1.persist(category1);
                 
                
                 category2= new Category();
 		category2.setName("Kitchen");
                 category2.addProduct(product2);
                 category2.addProduct(product3);
-		em.persist(category2);
+		em1.persist(category2);
                 
-                em.getTransaction().commit();
-		em.close();
+                product1 = new Product();
+                product1.setName("Flashlight");
+                product1.addCategory(category1);
+                em1.persist(product1);
+                product2 = new Product();
+                product2.setName("Kitchen robot");
+                product2.addCategory(category2);
+                em1.persist(product2);
+                product3 = new Product();
+                product3.setName("Plate");
+                product3.addCategory(category2);
+                em1.persist(product3);
+		em1.getTransaction().commit();
+		em1.close();
+                
         }
 	
        
@@ -96,11 +93,67 @@ public class Task02 extends AbstractTestNGSpringContextTests {
 		em_task01.getTransaction().begin();
 		
                 Category c1 = em_task01.find(Category.class, category1.getId());
-                
-                //Assert.assertEquals(c1.getName(), category1.getName());
-                this.assertContainsProductWithName(c1.getProducts(), "Flashlight");
-                
-                
+               
+                Assert.assertEquals(c1.getName(), category1.getName());
+                this.assertContainsProductWithName(c1.getProducts(), "Flashlight");           
+		em_task01.close();
+	}
+        
+        
+        @Test
+	public void category2Test() {
+		
+                EntityManager em_task01 = emf.createEntityManager();
+		em_task01.getTransaction().begin();
+		
+                Category c2 = em_task01.find(Category.class, category2.getId());
+               
+                Assert.assertEquals(c2.getName(), category2.getName());
+                this.assertContainsProductWithName(c2.getProducts(), "Kitchen robot");  
+                 this.assertContainsProductWithName(c2.getProducts(), "Plate");  
+		em_task01.close();
+	}
+        
+        
+        @Test
+	public void product1Test() {
+		
+                EntityManager em_task01 = emf.createEntityManager();
+		em_task01.getTransaction().begin();
+		
+                Product p1 = em_task01.find(Product.class, product1.getId());
+               
+                Assert.assertEquals(p1.getName(), product1.getName());
+                this.assertContainsCategoryWithName(p1.getCategories(), "Electro");  
+              
+		em_task01.close();
+	}
+        
+         @Test
+	public void product2Test() {
+		
+                EntityManager em_task01 = emf.createEntityManager();
+		em_task01.getTransaction().begin();
+		
+                Product p2 = em_task01.find(Product.class, product2.getId());
+               
+                Assert.assertEquals(p2.getName(), product2.getName());
+                this.assertContainsCategoryWithName(p2.getCategories(), "Kitchen");  
+              
+		em_task01.close();
+	}
+        
+         @Test
+	public void product3Test() {
+		
+                EntityManager em_task01 = emf.createEntityManager();
+		em_task01.getTransaction().begin();
+		
+                Product p3 = em_task01.find(Product.class, product3.getId());
+               
+                Assert.assertEquals(p3.getName(), product3.getName());
+                this.assertContainsCategoryWithName(p3.getCategories(), "Kitchen");  
+              
 		em_task01.close();
 	}
 
